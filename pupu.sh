4cf4df39ad59f67d60b2ff7494e98b8f425221d1
sukigsx <sukigsx.mbsistemas@gmail.com>
2024-05-22 13:22:16 +0200

    a

--- pupu.sh
+++
@@ -1,8 +0,0 @@
- #!/bin/bash
-
- read -p " Dime nombre y direccion > " nombre direccion
- if [[ $direccion == *":"* ]]; then
-    echo "La dirección es $direccion y contiene ':'"
-else
-    echo "La dirección es $direccion y no contiene ':'"
-fi
