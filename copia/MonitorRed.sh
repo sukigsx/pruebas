09a0e98843d78fd5d9ed51d1d08dc9dc67f5dc0f
sukigsx <sukigsx.mbsistemas@gmail.com>
2024-06-28 10:31:27 +0200

    a

---
+++ copia/MonitorRed.sh
@@ -0,0 +1,260 @@
+#!/bin/bash
+
+#VARIABLES PRINCIPALES
+# con export son las variables necesarias para exportar al los siguientes script
+#variables para el menu_info
+
+export NombreScript="MonitorRed"
+export DescripcionDelScript="Comprobar Dominios, Ips y Servicios activos en tu red "
+export Correo="scripts@mbbsistemas.es"
+export Web="https://repositorio.mbbsistemas.es"
+export version="1.aaaaaaaaaaaa"
+conexion="Sin comprobar"
+software="Sin comprobar"
+actualizado="No se ha podido comprobar la actualizacion del script"
+
+# VARIABLE QUE RECOJEN LAS RUTAS
+ruta_ejecucion=$(dirname "$(readlink -f "$0")") #es la ruta de ejecucion del script sin la / al final
+ruta_escritorio=$(xdg-user-dir DESKTOP) #es la ruta de tu escritorio sin la / al final
+
+# VARIABLES PARA LA ACTUALIZAION CON GITHUB
+NombreScriptActualizar="MonitorRed.sh" #contiene el nombre del script para poder actualizar desde github
+DireccionGithub="https://github.com/sukigsx/pruebas.git" #contiene la direccion de github para actualizar el script
+
+#VARIABLES DE SOFTWARE NECESARIO
+software="ping which git diff tail sed figlet curl" #contiene el software necesario separado por espacios
+
+
+#colores
+rojo="\e[0;31m\033[1m" #rojo
+verde="\e[;32m\033[1m"
+azul="\e[0;34m\033[1m"
+amarillo="\e[0;33m\033[1m"
+rosa="\e[0;35m\033[1m"
+turquesa="\e[0;36m\033[1m"
+borra_colores="\033[0m\e[0m" #borra colores
+
+
+# FUNCIONES
+
+#toma el control al pulsar control + c
+trap ctrl_c INT
+function ctrl_c()
+{
+clear
+echo ""
+echo -e "${azul} Gracias ${rosa}$(whoami)${azul}. Por utilizar mi script.${borra_colores}"
+echo ""
+sleep 1
+exit
+}
+
+
+menu_info(){
+# muestra el menu de sukigsx
+clear
+echo ""
+echo -e "${rosa}            _    _                  ${azul}   Nombre del script ( ${borra_colores}$NombreScript${azul} )${borra_colores}"
+echo -e "${rosa}  ___ _   _| | _(_) __ _ _____  __  ${azul}   Descripcion ( ${borra_colores}$DescripcionDelScript${azul})${borra_colores}"
+echo -e "${rosa} / __| | | | |/ / |/ _\ / __\ \/ /  ${azul}   Version            =${borra_colores} $version"
+echo -e "${rosa} \__ \ |_| |   <| | (_| \__ \>  <   ${azul}   Conexion Internet  =${borra_colores} $conexion"
+echo -e "${rosa} |___/\__,_|_|\_\_|\__, |___/_/\_\  ${azul}   Software necesario =${borra_colores} $software"
+echo -e "${rosa}                  |___/             ${azul}   Actualizado        =${borra_colores} $actualizado"
+echo -e ""
+echo -e "${azul} Contacto: ( ${borra_colores}Correo $Correo${azul} ) ( ${borra_colores}Web $Web${azul} )${borra_colores}"
+echo ""
+echo -e "${azul} Estado de configuracion del script:${borra_colores}"
+echo ""
+echo -e "${azul}    Servicios${borra_colores} =${amarillo} $configurado_servicios${borra_colores}"
+echo -e "${azul}    Ips      ${borra_colores} =${amarillo} $configurado_ips${borra_colores}"
+echo -e "${azul}    Dominios ${borra_colores} =${amarillo} $configurado_dominios${borra_colores}"
+echo ""
+}
+
+menu_inicial(){
+echo ""
+echo -e "${rosa}            _    _                  ${azul}   "
+echo -e "${rosa}  ___ _   _| | _(_) __ _ _____  __  ${azul}   "
+echo -e "${rosa} / __| | | | |/ / |/ _\ / __\ \/ /  ${azul}   Nombre del script ( ${borra_colores}$NombreScript${azul} )${borra_colores}"
+echo -e "${rosa} \__ \ |_| |   <| | (_| \__ \>  <   ${azul}   Descripcion ( ${borra_colores}$DescripcionDelScript${azul})${borra_colores}"
+echo -e "${rosa} |___/\__,_|_|\_\_|\__, |___/_/\_\  ${azul}"
+echo -e "${rosa}                  |___/             ${azul}"
+echo -e ""
+echo -e "${azul} Contacto: ( ${borra_colores}Correo $Correo${azul} ) ( ${borra_colores}Web $Web${azul} )${borra_colores}"
+echo ""
+}
+
+actualizar_script(){
+# actualizar el script
+#para que esta funcion funcione necesita:
+#   conexion a internet
+#   la paleta de colores
+#   software: git diff
+
+git clone $DireccionGithub /tmp/comprobar >/dev/null 2>&1
+
+diff $ruta_ejecucion/$NombreScriptActualizar /tmp/comprobar/$NombreScriptActualizar >/dev/null 2>&1
+
+
+if [ $? = 0 ]
+then
+    #esta actualizado, solo lo comprueba
+    echo ""
+    echo -e "${verde} El script${borra_colores} $0 ${verde}esta actualizado.${borra_colores}"
+    echo ""
+    chmod -R +w /tmp/comprobar
+    rm -R /tmp/comprobar
+    actualizado="SI"
+    sleep 2
+else
+    #hay que actualizar, comprueba y actualiza
+    echo ""
+    echo -e "${amarillo} EL script${borra_colores} $0 ${amarillo}NO esta actualizado.${borra_colores}"
+    echo -e "${verde} Se procede a su actualizacion automatica.${borra_colores}"
+    sleep 3
+    cp -r /tmp/comprobar/* $ruta_ejecucion
+    chmod -R +w /tmp/comprobar
+    rm -R /tmp/comprobar
+    echo ""
+    echo -e "${amarillo} El script se ha actualizado, es necesario cargarlo de nuevo.${borra_colores}"
+    echo ""
+    sleep 2
+    exit
+fi
+}
+
+#carga el fichero de configurado si o no (configuradosino) que contiene las variables para saber lo que esta configurado.
+source $ruta_ejecucion/MonitorRed/configuradosino
+
+software_necesario(){
+#funcion software necesario
+#para que funcione necesita:
+#   conexion a internet
+#   la paleta de colores
+#   software: which
+
+echo ""
+echo -e "${azul} Comprobando el software necesario.${borra_colores}"
+echo ""
+#which git diff ping figlet xdotool wmctrl nano fzf
+#########software="which git diff ping figlet nano gdebi curl konsole" #ponemos el foftware a instalar separado por espacion dentro de las comillas ( soft1 soft2 soft3 etc )
+for paquete in $software
+do
+which $paquete 2>/dev/null 1>/dev/null 0>/dev/null #comprueba si esta el programa llamado programa
+sino=$? #recojemos el 0 o 1 del resultado de which
+contador="1" #ponemos la variable contador a 1
+    while [ $sino -gt 0 ] #entra en el bicle si variable programa es 0, no lo ha encontrado which
+    do
+        if [ $contador = "4" ] || [ $conexion = "no" ] 2>/dev/null 1>/dev/null 0>/dev/null #si el contador es 4 entre en then y sino en else
+        then #si entra en then es porque el contador es igual a 4 y no ha podido instalar o no hay conexion a internet
+            clear
+            echo ""
+            echo -e " ${amarillo}NO se ha podido instalar ${rojo}$paquete${amarillo}.${borra_colores}"
+            echo -e " ${amarillo}Intentelo usted con la orden: (${borra_colores}sudo apt install $paquete ${amarillo})${borra_colores}"
+            echo -e ""
+            echo -e " ${rojo}No se puede ejecutar el script sin el software necesario.${borra_colores}"
+            echo ""; read p
+            echo ""
+            exit
+        else #intenta instalar
+            echo " Instalando $paquete. Intento $contador/3."
+            sudo apt install $paquete -y 2>/dev/null 1>/dev/null 0>/dev/null
+            let "contador=contador+1" #incrementa la variable contador en 1
+            which $paquete 2>/dev/null 1>/dev/null 0>/dev/null #comprueba si esta el programa en tu sistema
+            sino=$? ##recojemos el 0 o 1 del resultado de which
+        fi
+    done
+echo -e " [${verde}ok${borra_colores}] $paquete."
+software="SI"
+done
+echo ""
+echo -e "${azul} Todo el software ${verde}OK${borra_colores}"
+sleep 2
+}
+
+
+conexion(){
+#funcion de comprobar conexion a internet
+#para que funciones necesita:
+#   conexion ainternet
+#   la paleta de colores
+#   software: ping
+
+if ping -c1 google.com &>/dev/null
+then
+    conexion="SI"
+    echo ""
+    echo -e " Conexion a internet = ${verde}SI${borra_colores}"
+else
+    conexion="NO"
+    echo ""
+    echo -e " Conexion a internet = ${rojo}NO${borra_colores}"
+fi
+}
+
+#logica de arranque
+#variables de resultado $conexion $software $actualizado
+#funciones actualizar_script, conexion, software_necesario
+
+#logica para ejecutar o no ejecutar
+#comprobado conexcion
+#    si=actualizar_script
+#        si=software_necesario
+#            si=ejecuta, poner variables a sii todo
+#            no=Ya sale el solo desde la funcion
+#        no=software_necesario
+#            si=ejecuta, variables software="SI", conexion="SI", actualizado="No se ha podiso comprobar actualizacion de script"
+#            no=Ya sale solo desde la funcion
+#
+#    no=software_necesario
+#        si=ejecuta, variables software="SI", conexion="NO", actualizado="No se ha podiso comprobar actualizacion de script"
+#        no=Ya sale solo desde la funcion
+
+
+clear
+menu_inicial
+conexion
+if [ $conexion = "SI" ]; then
+    actualizar_script
+    if [ $actualizado = "SI" ]; then
+        software_necesario
+        if [ $software = "SI" ]; then
+            export software="SI"
+            export conexion="SI"
+            export actualizado="SI"
+            #bash $ruta_ejecucion/ #PON LA RUTA
+        else
+            echo ""
+        fi
+    else
+        software_necesario
+        if [ $software = "SI" ]; then
+            export software="SI"
+            export conexion="NO"
+            export actualizado="No se ha podido comprobar la actualizacion del script"
+            #bash $ruta_ejecucion/ #PON LA RUTA
+        else
+            echo ""
+        fi
+    fi
+else
+    software_necesario
+    if [ $software = "SI" ]; then
+        export software="SI"
+        export conexion="NO"
+        export actualizado="No se ha podido comprobar la actualizacion del script"
+        #bash $ruta_ejecucion/ #PON LA RUTA
+    else
+        echo ""
+    fi
+fi
+
+
+#ejecuta el siguiente codigo si no existe ninguna configuracion
+menu_info
+if [ "$configurado_servicios" = "si" ] || [ "$configurado_ips" = "si" ] || [ "$configurado_dominios" = "si" ]; then
+    bash $ruta_ejecucion/MonitorRed/MonitorRed
+else
+    bash $ruta_ejecucion/MonitorRed/configuracion
+fi
+
