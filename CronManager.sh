dd32cd60ee79c5f0ea06461a8eaad5ce9247775d
sukigsx <sukigsx.mbsistemas@gmail.com>
2025-12-04 12:17:39 +0100

    a

--- CronManager.sh
+++ CronManager.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
 
+#VARIABLES PRINCIPALES
+# con export son las variables necesarias para exportar al los siguientes script
+#variables para el menu_info
+
+export NombreScript="Linux User Manager"
+export DescripcionDelScript="Herramienta configuracion usuarios, carpetas y permisos, configuracion samba"
+export Correo=""
+export Web=""
+export version="1.0aaasssaasskhbddddttdwwddaaaaaaaaaaaah"
+conexion="Sin comprobar"
+software="Sin comprobar"
+actualizado="No se ha podido comprobar la actualizacion del script"
+
+# VARIABLE QUE RECOJEN LAS RUTAS
+ruta_ejecucion=$(dirname "$(readlink -f "$0")") #es la ruta de ejecucion del script sin la / al final
+ruta_escritorio=$(xdg-user-dir DESKTOP) #es la ruta de tu escritorio sin la / al final
+
+# VARIABLES PARA LA ACTUALIZAION CON GITHUB
+NombreScriptActualizar="CronManager.sh" #contiene el nombre del script para poder actualizar desde github
+DireccionGithub="https://github.com/sukigsx/pruebas" #contiene la direccion de github para actualizar el script
+
 #VARIABLES DE SOFTWARE NECESARIO
 # Asociamos comandos con el paquete que los contiene [comando a comprobar]="paquete a instalar"
     declare -A requeridos
     requeridos=(
-        [which]="which"
         [git]="git"
         [nano]="nano"
         [curl]="curl"
-        [diff]="diff"
+        [konsole]="konsole"
+        [getfacl]="acl"
     )
-archivo_local="CronManager.sh" # Nombre del archivo local
-ruta_repositorio="https://github.com/sukigsx/pruebas.git" #ruta del repositorio para actualizar y clonar con git clone
 
-#toma el control al pulsar control + c
-trap ctrl_c INT
-function ctrl_c()
-{
-clear
-figlet -c Gracias por
-figlet -c utilizar mi
-figlet -c script
-wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
-exit
-}
 
 #colores
-#ejemplo: echo -e "${verde} La opcion (-e) es para que pille el color.${borra_colores}"
 rojo="\e[0;31m\033[1m" #rojo
 verde="\e[;32m\033[1m"
 azul="\e[0;34m\033[1m"
@@ -35,39 +42,72 @@ rosa="\e[0;35m\033[1m"
 turquesa="\e[0;36m\033[1m"
 borra_colores="\033[0m\e[0m" #borra colores
 
-actualizar_script(){
-# Obtener la ruta del script
-descarga=$(dirname "$(readlink -f "$0")")
-git clone $ruta_repositorio /tmp/comprobar >/dev/null 2>&1
-
-diff $descarga/$archivo_local /tmp/comprobar/$archivo_local >/dev/null 2>&1
-
-
-if [ $? = 0 ]
-then
-    #esta actualizado, solo lo comprueba
-    echo ""
-    echo -e "${verde} El script${borra_colores} $0 ${verde}esta actualizado.${borra_colores}"
-    echo ""
-    chmod -R +w /tmp/comprobar
-    rm -R /tmp/comprobar
-    actualizado="SI"
-else
-    #hay que actualizar, comprueba y actualiza
-    echo ""
-    echo -e "${amarillo} EL script${borra_colores} $0 ${amarillo}NO esta actualizado.${borra_colores}"
-    echo -e "${verde} Se procede a su actualizacion automatica.${borra_colores}"
-    sleep 3
-    mv /tmp/comprobar/$archivo_local $descarga
-    chmod -R +w /tmp/comprobar
-    rm -R /tmp/comprobar
-    echo ""
-    echo -e "${amarillo} El script se ha actualizado, es necesario cargarlo de nuevo.${borra_colores}"
-    echo -e ""
-    read -p " Pulsa una tecla para continuar." pause
-    exit
-fi
+#toma el control al pulsar control + c
+trap ctrl_c INT
+function ctrl_c()
+{
+clear
+echo ""
+echo -e "${azul} GRACIAS POR UTILIZAR MI SCRIPT${borra_colores}"
+echo ""
+sleep 1
+exit
 }
+
+menu_info(){
+# muestra el menu de sukigsx
+echo ""
+echo -e "${rosa}            _    _                  ${azul}   Nombre del script${borra_colores} ($NombreScript)"
+echo -e "${rosa}  ___ _   _| | _(_) __ _ _____  __  ${azul}   Descripcion${borra_colores} ($DescripcionDelScript)"
+echo -e "${rosa} / __| | | | |/ / |/ _\ / __\ \/ /  ${azul}   Version            =${borra_colores} $version"
+echo -e "${rosa} \__ \ |_| |   <| | (_| \__ \>  <   ${azul}   Conexion Internet  =${borra_colores} $conexion"
+echo -e "${rosa} |___/\__,_|_|\_\_|\__, |___/_/\_\  ${azul}   Software necesario =${borra_colores} $software"
+echo -e "${rosa}                  |___/             ${azul}   Actualizado        =${borra_colores} $actualizado"
+echo -e ""
+echo -e "${azul} Contacto:${borra_colores} (Correo $Correo) (Web $Web)${borra_colores}"
+echo ""
+}
+
+
+actualizar_script(){
+    # actualizar el script
+    #para que esta funcion funcione necesita:
+    #   conexion a internet
+    #   la paleta de colores
+    #   software: git diff
+
+    git clone $DireccionGithub /tmp/comprobar >/dev/null 2>&1
+
+    diff $ruta_ejecucion/$NombreScriptActualizar /tmp/comprobar/$NombreScriptActualizar >/dev/null 2>&1
+
+
+    if [ $? = 0 ]
+    then
+        #esta actualizado, solo lo comprueba
+        echo ""
+        echo -e "${verde} El script${borra_colores} $0 ${verde}esta actualizado.${borra_colores}"
+        echo ""
+        chmod -R +w /tmp/comprobar
+        rm -R /tmp/comprobar
+        actualizado="SI"
+        sleep 2
+    else
+        #hay que actualizar, comprueba y actualiza
+        echo ""
+        echo -e "${amarillo} EL script${borra_colores} $0 ${amarillo}NO esta actualizado.${borra_colores}"
+        echo -e "${verde} Se procede a su actualizacion automatica.${borra_colores}"
+        sleep 3
+        cp -r /tmp/comprobar/* $ruta_ejecucion
+        chmod -R +w /tmp/comprobar
+        rm -R /tmp/comprobar
+        echo ""
+        echo -e "${amarillo} El script se ha actualizado, es necesario cargarlo de nuevo.${borra_colores}"
+        echo ""
+        sleep 2
+        exit
+    fi
+}
+
 
 software_necesario(){
 #funcion software necesario
@@ -111,5 +151,82 @@ for comando in "${!requeridos[@]}"; do
     echo -e "${azul} Todo el software ${verde}OK${borra_colores}"
     sleep 2
 }
-actualizar_script
 
+
+conexion(){
+#funcion de comprobar conexion a internet
+#para que funciones necesita:
+#   conexion ainternet
+#   la paleta de colores
+#   software: ping
+
+if ping -c1 google.com &>/dev/null
+then
+    conexion="SI"
+    echo ""
+    echo -e " Conexion a internet = ${verde}SI${borra_colores}"
+else
+    conexion="NO"
+    echo ""
+    echo -e " Conexion a internet = ${rojo}NO${borra_colores}"
+fi
+}
+
+#logica de arranque
+#variables de resultado $conexion $software $actualizado
+#funciones actualizar_script, conexion, software_necesario
+
+#logica para ejecutar o no ejecutar
+#comprobado conexcion
+#    si=actualizar_script
+#        si=software_necesario
+#            si=ejecuta, poner variables a sii todo
+#            no=Ya sale el solo desde la funcion
+#        no=software_necesario
+#            si=ejecuta, variables software="SI", conexion="SI", actualizado="No se ha podiso comprobar actualizacion de script"
+#            no=Ya sale solo desde la funcion
+#
+#    no=software_necesario
+#        si=ejecuta, variables software="SI", conexion="NO", actualizado="No se ha podiso comprobar actualizacion de script"
+#        no=Ya sale solo desde la funcion
+
+
+clear
+menu_info
+conexion
+if [ $conexion = "SI" ]; then
+    actualizar_script
+    if [ $actualizado = "SI" ]; then
+        software_necesario
+        if [ $software = "SI" ]; then
+            export software="SI"
+            export conexion="SI"
+            export actualizado="SI"
+            #bash $ruta_ejecucion/ #PON LA RUTA
+        else
+            echo ""
+        fi
+    else
+        software_necesario
+        if [ $software = "SI" ]; then
+            export software="SI"
+            export conexion="NO"
+            export actualizado="No se ha podido comprobar la actualizacion del script"
+            #bash $ruta_ejecucion/ #PON LA RUTA
+        else
+            echo ""
+        fi
+    fi
+else
+    software_necesario
+    if [ $software = "SI" ]; then
+        export software="SI"
+        export conexion="NO"
+        export actualizado="No se ha podido comprobar la actualizacion del script"
+        #bash $ruta_ejecucion/ #PON LA RUTA
+    else
+        echo ""
+    fi
+fi
+
+echo "continuo ejecutando"
