e55397884b44251d1b80ff59e059c3c272534d17
sukigsx <sukigsx.mbsistemas@gmail.com>
2025-12-04 12:10:52 +0100

    Create CronManager.sh

---
+++ CronManager.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+#VARIABLES DE SOFTWARE NECESARIO
+# Asociamos comandos con el paquete que los contiene [comando a comprobar]="paquete a instalar"
+    declare -A requeridos
+    requeridos=(
+        [which]="which"
+        [git]="git"
+        [nano]="nano"
+        [curl]="curl"
+        [diff]="diff"
+    )
+archivo_local="CronManager.sh" # Nombre del archivo local
+ruta_repositorio="https://github.com/sukigsx/pruebas.git" #ruta del repositorio para actualizar y clonar con git clone
+
+actualizar_script(){
+# Obtener la ruta del script
+descarga=$(dirname "$(readlink -f "$0")")
+git clone $ruta_repositorio /tmp/comprobar >/dev/null 2>&1
+
+diff $descarga/$archivo_local /tmp/comprobar/$archivo_local >/dev/null 2>&1
+
+
+if [ $? = 0 ]
+then
+    #esta actualizado, solo lo comprueba
+    echo ""
+    echo -e "${verde} El script${borra_colores} $0 ${verde}esta actualizado.${borra_colores}"
+    echo ""
+    chmod -R +w /tmp/comprobar
+    rm -R /tmp/comprobar
+    actualizado="SI"
+else
+    #hay que actualizar, comprueba y actualiza
+    echo ""
+    echo -e "${amarillo} EL script${borra_colores} $0 ${amarillo}NO esta actualizado.${borra_colores}"
+    echo -e "${verde} Se procede a su actualizacion automatica.${borra_colores}"
+    sleep 3
+    mv /tmp/comprobar/$archivo_local $descarga
+    chmod -R +w /tmp/comprobar
+    rm -R /tmp/comprobar
+    echo ""
+    echo -e "${amarillo} El script se ha actualizado, es necesario cargarlo de nuevo.${borra_colores}"
+    echo -e ""
+    read -p " Pulsa una tecla para continuar." pause
+    exit
+fi
+}
+
+software_necesario(){
+#funcion software necesario
+#para que funcione necesita:
+#   conexion a internet
+#   la paleta de colores
+#   software: which
+
+echo ""
+echo -e "${azul} Comprobando el software necesario.${borra_colores}"
+echo ""
+#which git diff ping figlet xdotool wmctrl nano fzf
+#########software="which git diff ping figlet nano gdebi curl konsole" #ponemos el foftware a instalar separado por espacion dentro de las comillas ( soft1 soft2 soft3 etc )
+for comando in "${!requeridos[@]}"; do
+        which $comando &>/dev/null
+        sino=$?
+        contador=1
+        while [ $sino -ne 0 ]; do
+            if [ $contador -ge 4 ] || [ "$conexion" = "no" ]; then
+                clear
+                echo ""
+                echo -e " ${amarillo}NO se ha podido instalar ${rojo}${requeridos[$comando]}${amarillo}.${borra_colores}"
+                echo -e " ${amarillo}Inténtelo usted con: (${borra_colores}sudo apt install ${requeridos[$comando]}${amarillo})${borra_colores}"
+                echo -e ""
+                echo -e " ${rojo}No se puede ejecutar el script sin el software necesario.${borra_colores}"
+                echo ""; read p
+                echo ""
+                exit 1
+            else
+                echo " Instalando ${requeridos[$comando]}. Intento $contador/3."
+                sudo apt install ${requeridos[$comando]} -y &>/dev/null
+                let "contador=contador+1"
+                which $comando &>/dev/null
+                sino=$?
+            fi
+        done
+        echo -e " [${verde}ok${borra_colores}] $comando (${requeridos[$comando]})."
+    done
+
+    echo ""
+    echo -e "${azul} Todo el software ${verde}OK${borra_colores}"
+    sleep 2
+}
+software_necesario
