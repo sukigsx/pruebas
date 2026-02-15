68899cd9eef85d1753527e766e686587861f063c
sukigsx <sukigsx.mbsistemas@gmail.com>
2026-02-15 08:38:20 +0100

    Update popo.sh

--- popo.sh
+++ popo.sh
@@ -28,7 +28,7 @@ DireccionGithub="https://github.com/sukigsx/pruebas" #contiene la direccion de g
         [git]="git"
         [nano]="nano"
         [curl]="curl"
-        [vlc]="vlc"
+        #[vlc]="vlc"
     )
 
 
