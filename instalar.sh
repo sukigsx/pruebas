dec8e2df518e97bb281697ff0b56151fa3893fab
sukigsx <sukigsx.mbsistemas@gmail.com>
2026-02-16 10:14:12 +0100

    aa

--- instalar.sh
+++ instalar.sh
@@ -37,7 +37,7 @@ fi
 software_necesario(){
 var_software="no"
 echo -e " Verificando software necesario:"
-software="which git diff ping fzf" #ponemos el foftware a instalar separado por espacion dentro de las comillas ( soft1 soft2 soft3 etc )
+software="which git diff ping apt fzf" #ponemos el foftware a instalar separado por espacion dentro de las comillas ( soft1 soft2 soft3 etc )
 for paquete in $software
 do
 which $paquete 2>/dev/null 1>/dev/null 0>/dev/null #comprueba si esta el programa llamado programa
